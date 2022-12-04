494460a52b6254fdf1ac052980093fed489f4138
Luis Arthur <luisarthir259@gmail.com>
2022-12-04 12:16:30 -0300

    express-examples

---
+++ node-express/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const http = require('http')
+const hostname = 'localhost'
+const port = 3000
+const app = express()
+const server = http.createServer(app)
+app.use((req, res , next) => {
+    console.log(req.headers)
+    req.statusCode = 200
+    res.setHeader('Content-Type','text/html')
+    res.end('<html><body><h1>This is an Express Server </h1></body></html>')
+})
+
+server.listen(port,hostname, ()=> {
+    console.log(`Server is running at http://${hostname}:${port}`)
+})
