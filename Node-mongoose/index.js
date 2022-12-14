ded7fd3fff7ca78795287b4b2aec9a75aa68c856
Luis Arthur <luisarthir259@gmail.com>
2022-12-14 13:15:16 -0300

    Node mongose part 2

--- Node-mongoose/index.js
+++ Node-mongoose/index.js
@@ -8,27 +8,41 @@ const connect = mongoose.connect(url);
 
 connect.then((db) => {
     console.log('Connected correctly to server');
-    
-    var newDish = Dishes({
-        name:'Uthappizza',
+
+    Dishes.create({
+        name: 'Uthappizza',
         description: 'test'
     })
-
-    newDish.save()
     .then((dish) => {
         console.log(dish);
 
-        return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
+    .then((dish) => {
+        console.log(dish);
 
-        return Dishes.remove({})
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.remove({});
     })
     .then(() => {
         return mongoose.connection.close();
     })
     .catch((err) => {
         console.log(err);
-    })
-})
+    });
+});
