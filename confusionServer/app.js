143ed00d48d92a851379b0c2a2db99c79c64fa67
Luis Arthur <luisarthir259@gmail.com>
2022-12-24 14:00:14 -0300

    passport

--- confusionServer/app.js
+++ confusionServer/app.js
@@ -4,8 +4,11 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var mongoose = require('mongoose')
-var session =  require('express-session')
+var session = require('express-session')
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate.js')
+
 
 //importing files modules
 var dishRouter = require('./routes/dishRouter.js');
@@ -13,7 +16,7 @@ var promoRouter = require('./routes/promoRouter.js');
 var leadersRouter = require('./routes/leadersRouter.js');
 var indexRouter = require('./routes/index.js');
 var usersRouter = require('./routes/users.js');
-const { Session } = require('inspector');
+
 
 // mongoDb server configuration 
 const url = 'mongodb://127.0.0.1:27017/confusion';
@@ -30,75 +33,44 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-//app sets
+//seting Server sets/routes
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leadersRouter);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// express sessions
 app.use(session({
   name: 'session-id',
-  secret: '1234567890-0987654321',
+  secret: '1234567890-09',
   saveUninitialized: false,
   resave: false,
   store: new FileStore()
 }));
+app.use(passport.initialize());
+app.use(passport.session())
+app.use('/', indexRouter);
+app.use('/users', usersRouter)
 
-
-//basic authentication with cookies 
-function auth (req, res, next) {
-  console.log(req.session);
-
-  if (!req.session.user) {
-      var authHeader = req.headers.authorization;
-      if (!authHeader) {
-          var err = new Error('You are not authenticated!');
-          res.setHeader('WWW-Authenticate', 'Basic');                        
-          err.status = 401;
-          next(err);
-          return;
-      }
-      var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-      var user = auth[0];
-      var pass = auth[1];
-      if (user == 'admin' && pass == 'password') {
-          req.session.user = 'admin';
-          next(); // authorized
-      } else {
-          var err = new Error('You are not authenticated!');
-          res.setHeader('WWW-Authenticate', 'Basic');
-          err.status = 401;
-          next(err);
-      }
+function auth(req, res, next) {
+  console.log(req.User);
+  
+  if (!req.User) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
   }
   else {
-      if (req.session.user === 'admin') {
-          console.log('req.session: ',req.session);
-          next();
-      }
-      else {
-          var err = new Error('You are not authenticated!');
-          err.status = 401;
-          next(err);
-      }
+        next();
   }
 }
-
 app.use(auth)
 app.use(express.static(path.join(__dirname, 'public')));
-
-//seting Server routes
-app.use('/dishes', dishRouter);
-app.use('/promotions', promoRouter);
-app.use('/leaders', leadersRouter);
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
-
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
