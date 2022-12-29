2cfdcd27db228ec70d66d51a5d373c81473d097c
Luis Arthur <luisarthir259@gmail.com>
2022-12-29 12:36:49 -0300

    https

--- confusionServer/app.js
+++ confusionServer/app.js
@@ -47,14 +47,26 @@ app.use(passport.initialize());
 app.use(passport.session())
 
 // server  routes
+
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);0
-
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leadersRouter);
-
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
